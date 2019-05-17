8deea5f848bf3d113b70ab2cf0366d12a9caa434
Stephen Ibanez <sibanez@g.hmc.edu>
2019-05-17 11:49:32 -0700

    Updated setup script to load drivers and bind interfaces

--- setup.sh
+++ setup.sh
@@ -7,3 +7,9 @@ sudo insmod ${RTE_SDK}/${RTE_TARGET}/kmod/igb_uio.ko
 # setup hugepages
 mount -t hugetlbfs -o pagesize=1G nodev /mnt/huge
 
+# bind eth1 interface to DPDK drivers
+sudo ${RTE_SDK}/usertools/dpdk-devbind.py --bind=igb_uio eth1
+
+# bring up eth2
+sudo ifconfig eth2 up
+
